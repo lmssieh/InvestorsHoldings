8fcded6677097fc91b62f15ee6ec6aba283779a7
rust <53446488+lmssieh@users.noreply.github.com>
2022-10-23 00:00:46 +0100

    add windicss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "windi.css";
+import Footer from "../components/UI/Footer";
+import Navbar from "../components/UI/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className=" bg-dark-700 text-white px-4 ">
+			<div className="max-w-[1200px] font-mono flex flex-col justify-center min-h-[100vh] m-auto">
+				<Navbar />
+				<div className="flex-1">
+					<Component {...pageProps} />
+				</div>
+				<div className="mt-auto">
+					<Footer />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
